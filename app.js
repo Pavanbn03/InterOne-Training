da603a7e837d25146c62fbfdcbbcf09fcbb09be2
Pavanbn03 <pavanbn03@gmail.com>
2020-11-22 14:15:58 +0530

    Website Completed

---
+++ app.js
@@ -0,0 +1,71 @@
+const content1 = document.querySelector(".content-1");
+const btn1 = document.querySelector(".day-1");
+const content2 = document.querySelector(".content-2");
+const btn2 = document.querySelector(".day-2");
+const content3 = document.querySelector(".content-3");
+const btn3 = document.querySelector(".day-3");
+const content4 = document.querySelector(".content-4");
+const btn4 = document.querySelector(".day-4");
+const content5 = document.querySelector(".content-5");
+const btn5 = document.querySelector(".day-5");
+const content6 = document.querySelector(".content-6");
+const btn6 = document.querySelector(".day-6");
+const content7 = document.querySelector(".content-7");
+const btn7 = document.querySelector(".day-7");
+const content8 = document.querySelector(".content-8");
+const btn8 = document.querySelector(".day-8");
+const content9 = document.querySelector(".content-9");
+const btn9 = document.querySelector(".day-9");
+const content10 = document.querySelector(".content-10");
+const btn10 = document.querySelector(".day-10");
+const content11 = document.querySelector(".content-11");
+const btn11 = document.querySelector(".day-11");
+const content12 = document.querySelector(".content-12");
+const btn12 = document.querySelector(".day-12");
+const content13 = document.querySelector(".content-13");
+const btn13 = document.querySelector(".day-13");
+const content14 = document.querySelector(".content-14");
+const btn14 = document.querySelector(".day-14");
+
+btn1.addEventListener("click", () => {
+  content1.classList.toggle("revelContent");
+});
+btn2.addEventListener("click", () => {
+  content2.classList.toggle("revelContent");
+});
+btn3.addEventListener("click", () => {
+  content3.classList.toggle("revelContent");
+});
+btn4.addEventListener("click", () => {
+  content4.classList.toggle("revelContent");
+});
+btn5.addEventListener("click", () => {
+  content5.classList.toggle("revelContent");
+});
+btn6.addEventListener("click", () => {
+  content6.classList.toggle("revelContent");
+});
+btn7.addEventListener("click", () => {
+  content7.classList.toggle("revelContent");
+});
+btn8.addEventListener("click", () => {
+  content8.classList.toggle("revelContent");
+});
+btn9.addEventListener("click", () => {
+  content9.classList.toggle("revelContent");
+});
+btn10.addEventListener("click", () => {
+  content10.classList.toggle("revelContent");
+});
+btn11.addEventListener("click", () => {
+  content11.classList.toggle("revelContent");
+});
+btn12.addEventListener("click", () => {
+  content12.classList.toggle("revelContent");
+});
+btn13.addEventListener("click", () => {
+  content13.classList.toggle("revelContent");
+});
+btn14.addEventListener("click", () => {
+  content14.classList.toggle("revelContent");
+});
